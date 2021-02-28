2b7a7cbea7b99d587561e0c02ba327f35a5dcc8d
Jhonny Fialho <jhonny@hello-world.cc>
2021-02-28 19:26:07 -0300

    detail

--- resources/views/card/modal/detail.blade.php
+++ resources/views/card/modal/detail.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
     <div class="card-detail">
-        <h4>{{ $Card->card }}</h4>
+        <div class="container-fluid">
+            <h3 class="title">{{ $Card->card }}</h3>
+            <div class="row">
+                <div class="col-md-9">
+                    <h4 class="title-section">Descrição</h4>
+                    <textarea name="description" id="description" class="input" cols="30" rows="10" placeholder="Digite aqui uma descrição detalhada para o seu card"></textarea>
+                </div>
+                <div class="col-md-3">
+                    <h4 class="title-section">Ações</h4>
+                    <button class="btn">Membros</button>
+                    <button class="btn">Etiquetas</button>
+                    <button class="btn">Checklist</button>
+                    <button class="btn">Data de entrega</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
