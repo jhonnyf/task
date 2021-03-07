7ab365169f03893a701d2b1f7aad5656f16aeeb3
Jhonny Fialho <jhonny@hello-world.cc>
2021-03-07 20:19:10 -0300

    detail

--- resources/views/board/detail.blade.php
+++ resources/views/board/detail.blade.php
@@ -30,20 +30,7 @@
                             @endif
                         </div>
                     </div>
-
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            Action
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
-                    </div>
-
+                    
                 </div>
             </div>
         </div>
