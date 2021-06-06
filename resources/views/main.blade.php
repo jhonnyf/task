9792538ffee8873933f85d8340166c326f6ad439
Jhonny Fialho <jhonny@hello-world.cc>
2021-06-06 15:57:35 -0300

    ajustes fonts

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,8 +3,6 @@
     <head>    
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
         <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
         <title>TASK</title>
@@ -29,7 +27,7 @@
         </main>        
         <script src="{{ asset('js/vendor.js') }}"></script>
         <script src="{{ asset('js/main.js') }}"></script>
-        @if (getenv('APP_ENV') === 'local')
+        @if (getenv('APP_ENV') === 'locals')
             <script id="__bs_script__">//<![CDATA[
                 document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.14'><\/script>".replace("HOST", location.hostname));
             //]]></script>
